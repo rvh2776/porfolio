a50a9afb11a2ba523d92f1881763cb303b725b96
Rafael V.H <rafael.vh@gmail.com>
2024-09-17 17:08:52 -0300

    Agrego Proyecto 1 Front arreglo importacion

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,7 +4,7 @@ import HTMLFlipBook from 'react-pageflip';
 import SkillsPage from './SkillsPage/SkillsPage';
 import { NavBar } from './NavBar/NavBar';
 import './Book.css';
-import Projects from './projects/projects';
+import Projects from './Projects/Projects';
 
 // eslint-disable-next-line react/display-name
 const PageCover = React.forwardRef((props, ref) => {
